8a688dc1e1b88474a4bbfe24b90947ecad2a7671
Whth <88489697+Whth@users.noreply.github.com>
2023-05-11 19:22:15 +0800

    move module
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import os
 import sys
-import threading
+
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('./hotConfigure'))
 
 # 设置运行环境变量LD_LIBRARY_PATH，添加lib库搜索路径
 libdir = os.path.abspath(os.path.dirname(__file__))
@@ -10,7 +12,7 @@ from .uptech import *
 from .up_controller import *
 from .serial_helper import *
 from .close_loop_controller import *
-from .valueTest import *
+from .hotConfigure.valueTest import *
 
 __version__ = "1.0"
 __all__ = [
@@ -18,5 +20,5 @@ __all__ = [
     'up_controller',
     'serial_helper',
     'close_loop_controller',
-    'valueTest'
+    'hotConfigure'
 ]
