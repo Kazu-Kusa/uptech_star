af2388d57a9dd38ff6cf0fa227a9598165d4ab9f
Whth <88489697+Whth@users.noreply.github.com>
2023-05-20 18:07:37 +0800

    添加timer模块
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

---
+++ timer.py
@@ -0,0 +1,21 @@
+from time import perf_counter
+
+
+def delay_ms(milliseconds: int):
+    start = perf_counter()
+    while True:
+        elapsed = (perf_counter() - start) * 1000
+        if elapsed > milliseconds:
+            break
+
+
+def delay_us(microseconds: int):
+    start = perf_counter()
+    while True:
+        elapsed = (perf_counter() - start) * 1000000
+        if elapsed > microseconds:
+            break
+
+
+if __name__ == '__main__':
+    pass
