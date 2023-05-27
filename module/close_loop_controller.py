29d395811aa5560a1480fc3066575bf7a0e76262
Whth <88489697+Whth@users.noreply.github.com>
2023-05-27 21:14:04 +0800

    add msg sending delay,preventing msg jam
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/close_loop_controller.py
+++ module/close_loop_controller.py
@@ -1,19 +1,32 @@
 import threading
 from .serial_helper import SerialHelper
+from .timer import delay_us
 
 
 class CloseLoopController:
 
-    def __init__(self, motor_ids_list: tuple = (1, 2, 3, 4)):
-        # 创建串口对象
+    def __init__(self, motor_ids_list: tuple = (1, 2, 3, 4), sending_delay: int = 1):
+        """
 
+        :param motor_ids_list:
+        :param sending_delay:
+        """
+        # 创建串口对象
         self.serial = SerialHelper(con2port_when_created=True, auto_search_port=True)
         self.msg_send_thread = None
         # 发送的数据队列
         self.msg_list = []
         self.start_msg_sending()
+        self._sending_delay = sending_delay
+        self._motor_id_list = motor_ids_list
 
-        self.motor_id_list = motor_ids_list
+    @property
+    def sending_delay(self):
+        return self._sending_delay
+
+    @sending_delay.setter
+    def sending_delay(self, sending_delay: int):
+        self._sending_delay = sending_delay
 
     def start_msg_sending(self):
         # 通信线程创建启动
@@ -33,6 +46,7 @@ class CloseLoopController:
                 if print_info:
                     print(f'writing {temp} to channel,remaining {len(self.msg_list)}')
                 self.serial.write(temp)
+                delay_us(self.sending_delay)
 
     @staticmethod
     def makeCmd(cmd: str) -> bytes:
@@ -40,7 +54,7 @@ class CloseLoopController:
 
     def set_motors_speed(self, speed_list: list[int], id_list: tuple = (1, 2, 3, 4), debug: bool = False):
         if id_list is None:
-            id_list = self.motor_id_list
+            id_list = self._motor_id_list
         cmd_list = []
         for i, motor_id in enumerate(id_list):
             cmd_list.append(self.makeCmd(f'{motor_id}v{speed_list[i]}'))
