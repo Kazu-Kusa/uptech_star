40e2aa0b3a00df74bc27ce159f06c53190ad5130
Whth <88489697+Whth@users.noreply.github.com>
2023-05-29 23:40:04 +0800

    use perf_counter_ns
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/timer.py
+++ module/timer.py
@@ -1,17 +1,27 @@
-from time import perf_counter
+from time import perf_counter_ns
 
 
 def delay_ms(milliseconds: int):
-    start = perf_counter()
+    """
+
+    :param milliseconds:
+    :return:
+    """
+    start = perf_counter_ns()
     while True:
-        elapsed = (perf_counter() - start) * 1000
+        elapsed = (perf_counter_ns() - start) // 1000000
         if elapsed > milliseconds:
             break
 
 
 def delay_us(microseconds: int):
-    start = perf_counter()
+    """
+
+    :param microseconds:
+    :return:
+    """
+    start = perf_counter_ns()
     while True:
-        elapsed = (perf_counter() - start) * 1000000
+        elapsed = (perf_counter_ns() - start) // 1000
         if elapsed > microseconds:
             break
