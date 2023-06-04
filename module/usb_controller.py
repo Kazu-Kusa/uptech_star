54d6d6e66823b049728d614b434746e76451f6cb
Whth <88489697+Whth@users.noreply.github.com>
2023-06-04 15:12:52 +0800

    change method to specifically close uart device according location
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/usb_controller.py
+++ module/usb_controller.py
@@ -2,10 +2,21 @@ import subprocess
 import warnings
 
 
-def reload_all_usb_devices(shut_down_delay: int = 50, boot_delay: int = 500, print_details: bool = True) -> None:
+def reload_uart_device(shut_down_delay: int = 200, boot_delay: int = 400, print_details: bool = False) -> None:
+    """
+    primarily used to reset the uart device since it can't handle exceptions on run,
+    will repower the usb device using uhubctl
+    :param shut_down_delay:
+    :param boot_delay:
+    :param print_details:
+    :return:
+    """
     # 运行sudo命令
-    usb_hub_vid = '2109:3431'
-    command = f" sudo uhubctl -n {usb_hub_vid} -a 0 -w {shut_down_delay}&&sudo uhubctl -n {usb_hub_vid} -a 1 -w {boot_delay}"
+    device_location = '1-1'
+    port = '2'
+    command = f"sudo uhubctl -a 0 -p {port} -l {device_location} -w {shut_down_delay}&&" \
+              f"sudo rm -rf /dev/ttyUSB0&&" \
+              f"sudo uhubctl -a 1 -p {port} -l {device_location} -w {boot_delay}"
 
     if print_details:
         subprocess.call(command, shell=True)
