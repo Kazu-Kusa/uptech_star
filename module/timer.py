658abaf6d088bd1a1f6150396865cedd4eea8555
Whth <88489697+Whth@users.noreply.github.com>
2023-06-04 20:13:25 +0800

    timer add tools and fix delay_us bug
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/timer.py
+++ module/timer.py
@@ -10,6 +10,14 @@ def delay_ms(milliseconds: int):
 
 
 def delay_us(microseconds: int):
-    end = perf_counter_ns() + microseconds * 1000000
-    while perf_counter_ns() > end:
+    end = perf_counter_ns() + microseconds * 1000
+    while perf_counter_ns() < end:
         pass
+
+
+def current_ms():
+    return int(perf_counter_ns() / 1000000)
+
+
+def get_end_time_ms(milliseconds: int) -> int:
+    return perf_counter_ns() + milliseconds * 1000000
