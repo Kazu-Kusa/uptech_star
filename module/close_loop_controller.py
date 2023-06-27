c706d2b5c704edae5b8cf4f9af0a61c2486cedca
Whth <88489697+Whth@users.noreply.github.com>
2023-06-27 15:50:16 +0800

    set some properties to private
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/close_loop_controller.py
+++ module/close_loop_controller.py
@@ -4,7 +4,6 @@ import time
 from typing import List, Tuple, Optional, Union
 from .serial_helper import SerialHelper
 from .timer import delay_us
-from .db_tools import persistent_lru_cache
 from .constant import ENV_CACHE_DIR_PATH
 
 cache_dir = os.environ.get(ENV_CACHE_DIR_PATH)
@@ -12,7 +11,8 @@ cache_dir = os.environ.get(ENV_CACHE_DIR_PATH)
 
 class CloseLoopController:
 
-    def __init__(self, motor_ids_list: Tuple[int, int, int, int], sending_delay: int = 100, debug: bool = False):
+    def __init__(self, motor_ids_list: Tuple[int, int, int, int], port: Optional[str] = 'tty/USB0',
+                 sending_delay: int = 100, debug: bool = False):
         """
 
         :param motor_ids_list: the id of the motor,represent as follows [fl,rl,rr,fr]
@@ -20,15 +20,15 @@ class CloseLoopController:
         """
         self._debug = debug
         # 创建串口对象
-        self.serial = SerialHelper(con2port_when_created=True, auto_search_port=True)
-        self.msg_send_thread = None
+        self._serial = SerialHelper(port=port, con2port_when_created=True, auto_search_port=True)
+        self._msg_send_thread = None
         # 发送的数据队列
         self._motor_speed_list: Tuple[int, int, int, int] = (0, 0, 0, 0)
         self._sending_delay: int = sending_delay
         self._motor_id_list: Tuple[int, int, int, int] = motor_ids_list
-        self.msg_list = [self.makeCmd('RESET')]
+        self._msg_list = [self.makeCmd('RESET')]
 
-        self.start_msg_sending()
+        self._start_msg_sending()
 
     @property
     def motor_id_list(self) -> Tuple[int, int, int, int]:
@@ -54,13 +54,13 @@ class CloseLoopController:
     def sending_delay(self, sending_delay: int):
         self._sending_delay = sending_delay
 
-    def start_msg_sending(self) -> None:
+    def _start_msg_sending(self) -> None:
         # 通信线程创建启动
-        self.msg_send_thread = threading.Thread(name="msg_send_thread", target=self.msg_sending_thread)
-        self.msg_send_thread.daemon = True
-        self.msg_send_thread.start()
+        self._msg_send_thread = threading.Thread(name="msg_send_thread", target=self._msg_sending_loop)
+        self._msg_send_thread.daemon = True
+        self._msg_send_thread.start()
 
-    def msg_sending_thread(self) -> None:
+    def _msg_sending_loop(self) -> None:
         """
         串口通信线程发送函数
         :return:
@@ -69,16 +69,16 @@ class CloseLoopController:
 
         def sending_loop() -> None:
             while True:
-                if self.msg_list:
-                    self.serial.write(self.msg_list.pop(0))
+                if self._msg_list:
+                    self._serial.write(self._msg_list.pop(0))
                     delay_us(self.sending_delay)
 
         def sending_loop_debugging() -> None:
             while True:
-                if self.msg_list:
-                    temp = self.msg_list.pop(0)
-                    print(f'\nwriting {temp} to channel,remaining {len(self.msg_list)}')
-                    self.serial.write(temp)
+                if self._msg_list:
+                    temp = self._msg_list.pop(0)
+                    print(f'\nwriting {temp} to channel,remaining {len(self._msg_list)}')
+                    self._serial.write(temp)
                     delay_us(self.sending_delay)
 
         if self.debug:
@@ -129,12 +129,12 @@ class CloseLoopController:
                 cmd_list.append(f'{motor_id}v{speed * direction}')
 
             if cmd_list:
-                self.msg_list.append(self.makeCmd_list(cmd_list))
+                self._msg_list.append(self.makeCmd_list(cmd_list))
         self._motor_speed_list = speed_list
 
     def set_all_motors_speed(self, speed: int) -> None:
         # TODO: should check before setting
-        self.msg_list.append(self.makeCmd(f'v{speed}'))
+        self._msg_list.append(self.makeCmd(f'v{speed}'))
         self._motor_speed_list = [speed] * 4
 
     def set_all_motors_acceleration(self, acceleration: int) -> None:
@@ -145,7 +145,7 @@ class CloseLoopController:
         """
         assert 0 < acceleration < 30000, "Invalid acceleration value"
         # TODO: all sealed cmd should check if the desired value is valid
-        self.msg_list.append(self.makeCmd(f'ac{acceleration}'))
+        self._msg_list.append(self.makeCmd(f'ac{acceleration}'))
         self.eepSav()
 
     def eepSav(self) -> None:
@@ -155,7 +155,7 @@ class CloseLoopController:
         :return:
         """
         # TODO: pre-compile the 'eepsav' cmd to binary instead of doing compile each time on called
-        self.msg_list.append(self.makeCmd('eepsav'))
+        self._msg_list.append(self.makeCmd('eepsav'))
 
     def open_userInput_channel(self, debug: bool = False) -> None:
         """
@@ -168,13 +168,13 @@ class CloseLoopController:
         debug_temp = self.debug
         self.debug = debug
         if self.debug:
-            self.serial.start_read_thread()
+            self._serial.start_read_thread()
 
             def handler(data: bytes):
                 string = data.decode('ascii')
                 print(f'\nout[{ct}]: {string}')
 
-            self.serial.set_on_data_received_handler(handler)
+            self._serial.set_on_data_received_handler(handler)
         while True:
             user_input = input(f'\rin[{ct}]: ')
             ct += 1
@@ -184,7 +184,7 @@ class CloseLoopController:
                 self.debug = debug_temp
                 break
             else:
-                self.msg_list.append(self.makeCmd(user_input))
+                self._msg_list.append(self.makeCmd(user_input))
 
 
 def motor_speed_test(speed_level: int = 11, interval: float = 1, using_id: bool = True, laps: int = 3) -> None:
