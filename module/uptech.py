0247f42fe3c018043655febca326f3484f5a5237
Whth <88489697+Whth@users.noreply.github.com>
2023-07-07 21:08:42 +0800

    check todo convert some method to private
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/uptech.py
+++ module/uptech.py
@@ -2,6 +2,7 @@ import ctypes
 import os
 import threading
 from ctypes import cdll
+from ctypes import CDLL
 import warnings
 import pigpio
 
@@ -12,7 +13,7 @@ ld_library_path = os.environ.get(ENV_LIB_SO_PATH)
 
 
 @persistent_lru_cache(f'{ld_library_path}/lb_cache')
-def load_lib(libname: str) -> object:
+def load_lib(libname: str) -> CDLL:
     lib_file_name = f'{ld_library_path}/{libname}'
     print(f'Loading [{lib_file_name}]')
     return cdll.LoadLibrary(lib_file_name)
@@ -22,13 +23,12 @@ class UpTech:
     """
     provides sealed methods accessing to the IOs and builtin sensors
     """
-    # TODO: use argtypes and restype to mark the args type
-    # TODO: move this type def out of here
     try:
-        so_up = load_lib('libuptech.so')
-    except:
-        pass
-    _adc_data_list_type = ctypes.c_uint16 * 10
+        __lib = load_lib('libuptech.so')
+    except OSError:
+        warnings.warn('##Faild to load libuptech.so##')
+
+    __adc_data_list_type = ctypes.c_uint16 * 10
 
     __mpu_data_list_type = ctypes.c_float * 3
 
@@ -38,7 +38,7 @@ class UpTech:
                  using_updating_thread: bool = False):
         self.debug = debug
 
-        self._adc_all = self._adc_data_list_type()
+        self._adc_all = self.__adc_data_list_type()
         # 如果没有运行，则启动pigpio守护进程
 
         self.Pi = pigpio.pi()
@@ -80,33 +80,33 @@ class UpTech:
         """
         open the  adc-io plug
         """
-        return self.so_up.adc_io_open()
+        return self.__lib.adc_io_open()
 
     def ADC_IO_Close(self):
         """
         close the adc-io plug
         """
-        self.so_up.adc_io_close()
+        self.__lib.adc_io_close()
 
     @property
     def adc_all_channels(self):
         """
         get all adc channels and return they as a tuple
         """
-        self.so_up.ADC_GetAll(self._adc_all)
+        self.__lib.ADC_GetAll(self._adc_all)
         return self._adc_all
 
     def ADC_IO_SetIOLevel(self, index, level):
-        self.so_up.adc_io_Set(index, level)
+        self.__lib.adc_io_Set(index, level)
 
     def ADC_IO_SetAllIOLevel(self, value):
-        self.so_up.adc_io_SetAll(value)
+        self.__lib.adc_io_SetAll(value)
 
     def ADC_IO_SetAllIOMode(self, mode):
-        self.so_up.adc_io_ModeSetAll(mode)
+        self.__lib.adc_io_ModeSetAll(mode)
 
     def ADC_IO_SetIOMode(self, index, mode):
-        self.so_up.adc_io_ModeSet(index, mode)
+        self.__lib.adc_io_ModeSet(index, mode)
 
     @property
     def io_all_channels(self):
@@ -116,7 +116,7 @@ class UpTech:
         unsigned 8int
         """
 
-        return list([int(x) for x in f'{self.so_up.adc_io_InputGetAll():08b}'])
+        return list([int(x) for x in f'{self.__lib.adc_io_InputGetAll():08b}'])
 
     def MPU6500_Open(self, debug_info: bool = False):
         """
@@ -126,7 +126,7 @@ class UpTech:
             gyro: -+2000 degree/s
             sampling rate: 1kHz
         """
-        if self.so_up.mpu6500_dmp_init():
+        if self.__lib.mpu6500_dmp_init():
             warnings.warn('#failed to initialize MPU6500', category=RuntimeWarning)
         elif debug_info:
             warnings.warn('#MPU6500 successfully initialized')
@@ -136,7 +136,7 @@ class UpTech:
         """
         get the acceleration from MPU6500
         """
-        self.so_up.mpu6500_Get_Accel(self._accel_all)
+        self.__lib.mpu6500_Get_Accel(self._accel_all)
 
         return self._accel_all
 
@@ -145,7 +145,7 @@ class UpTech:
         """
         get gyro from MPU6500
         """
-        self.so_up.mpu6500_Get_Gyro(self._gyro_all)
+        self.__lib.mpu6500_Get_Gyro(self._gyro_all)
 
         return self._gyro_all
 
@@ -155,11 +155,11 @@ class UpTech:
         get attitude from MPU6500
         """
 
-        self.so_up.mpu6500_Get_Attitude(self._atti_all)
+        self.__lib.mpu6500_Get_Attitude(self._atti_all)
 
         return self._atti_all
 
-    def adc_io_sync_thread(self):
+    def __adc_io_sync_thread(self):
         """
         update data thread
         """
@@ -171,7 +171,7 @@ class UpTech:
         """
         open data update thread
         """
-        edge_thread = threading.Thread(name="adc_io_sync_thread", target=self.adc_io_sync_thread)
+        edge_thread = threading.Thread(name="adc_io_sync_thread", target=self.__adc_io_sync_thread)
         edge_thread.daemon = True
         edge_thread.start()
 
