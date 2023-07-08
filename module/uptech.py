28f5cef49cf9fa33e7b35b59fe76cd5d8fd5e0dc
Whth <88489697+Whth@users.noreply.github.com>
2023-07-08 16:26:14 +0800

    add more implementations
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/uptech.py
+++ module/uptech.py
@@ -14,34 +14,59 @@ from .db_tools import persistent_lru_cache
 ld_library_path = os.environ.get(ENV_LIB_SO_PATH)
 
 
+def build_watcher(sensor_update: Callable[..., Sequence[Any]],
+                  sensor_id: Tuple[int, ...],
+                  min_line: Optional[int],
+                  max_line: Optional[int] = None,
+                  args: Tuple = (),
+                  kwargs: Dict[str, Any] = {}) -> Callable[[], bool]:
+    """
+    构建传感器监视器函数。
+
+    Args:
+        sensor_update: 一个可调用对象，接受一个可选参数并返回一个序列。
+        sensor_id: 传感器的ID，为整数的元组。
+        min_line: 最小阈值，为整数。
+        max_line: 最大阈值，为整数，默认为None。
+        args: 可选参数的元组，默认为空元组。
+        kwargs: 可选关键字参数的字典，默认为空字典。
+
+    Returns:
+        返回一个没有参数且返回布尔值的可调用对象，用于监视传感器数据是否在阈值范围内。
+
+    Raises:
+        无异常抛出。
+
+    Example:
+        # 创建 SensorData 实例
+        sensor_data = SensorData()
+
+        # 将类的 @property 属性作为 sensor_update 参数传入构造器
+        test_watcher = build_watcher(sensor_data.sensor_update, (0, 1, 2), 5, 25)
+
+        # 调用生成的监视器函数
+        result = test_watcher()
+
+        print(result)  # 输出：True
+    """
+    if max_line and min_line:
+        def watcher() -> bool:
+            return all((max_line > sensor_update(*args, **kwargs)[x] > min_line) for x in sensor_id)
+    elif min_line:
+        def watcher() -> bool:
+            return all((sensor_update(*args, **kwargs)[x] > min_line) for x in sensor_id)
+    else:
+        def watcher() -> bool:
+            return all((sensor_update(*args, **kwargs)[x] < max_line) for x in sensor_id)
+
+    return watcher
+
+
 @persistent_lru_cache(f'{ld_library_path}/lb_cache')
 def load_lib(libname: str) -> CDLL:
     lib_file_name = f'{ld_library_path}/{libname}'
     print(f'Loading [{lib_file_name}]')
     return cdll.LoadLibrary(lib_file_name)
-
-
-def build_watcher(sensor_update: Callable[..., Sequence[Any]],
-                  sensor_id: Tuple[int, ...],
-                  min_line: int,
-                  max_line: int,
-                  args: Tuple = (),
-                  kwargs: Dict[str, Any] = {}) -> Callable[[], bool]:
-    """
-    构建传感器监视器
-    :param sensor_update: 一个可调用对象，接受一个可选参数并返回一个序列
-    :param sensor_id: 传感器的ID，为整数元组
-    :param min_line: 最小阈值，为整数
-    :param max_line: 最大阈值，为整数
-    :param args: 可选参数元组
-    :param kwargs: 可选关键字参数字典
-    :return: 返回一个没有参数且返回布尔值的可调用对象
-    """
-
-    def watcher() -> bool:
-        return all((max_line > sensor_update(*args, **kwargs)[x] > min_line) for x in sensor_id)
-
-    return watcher
 
 
 class UpTech:
