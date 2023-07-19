cf5afbd1629f82ae21554c984046ceac3e20c166
Whth <88489697+Whth@users.noreply.github.com>
2023-07-20 01:01:05 +0800

    :adjust change the namespace of some static methods
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/serial_helper.py
+++ module/serial_helper.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from typing import List, Callable, Any, Optional, ByteString
 import serial
 from serial import Serial
@@ -9,19 +9,19 @@ import threading
 
 
 class SerialHelper:
+    """
+    所有共享状态变量（如 _serial 和 _is_connected）的访问都添加了对应的锁获取和释放操作。
+
+    而使用 DummyLock 可以不影响程序性能并且可以消除后面调用锁时可能会出现的 NoneType Error 问题。
+    另外，在新代码中，我们新增了 start_read_thread 方法和 _on_data_received_handler 回调函数，
+    可以统一应对串口设备发送过来的数据，并且用户也可以使用 set_data_handler 方法通过传入回调函数的方式来自定义处理接收到的数据。
+    """
 
     def __init__(self, port: Optional[str] = None,
                  baud_rate: int = 115200, bytesize: int = 8, parity: str = 'N', stop_bits: int = 1,
                  con2port_when_created: bool = True, auto_search_port: bool = True):
         """
-        所有共享状态变量（如 _serial 和 _is_connected）的访问都添加了对应的锁获取和释放操作。
-        移除了 _on_connected_changed 和 _on_data_received 两个线程方法，
-        新增了 start_read_thread 线程来不断监听串口设备是否有数据返回。
-        新增 DummyLock 类作为在单线程场景下实际使用的锁占位符。
-        这是因为如果在单线程的环境中使用普通的锁会降低程序效率，
-        而使用 DummyLock 可以不影响程序性能并且可以消除后面调用锁时可能会出现的 NoneType Error 问题。
-        另外，在新代码中，我们新增了 start_read_thread 方法和 _on_data_received_handler 回调函数，
-        可以统一应对串口设备发送过来的数据，并且用户也可以使用 set_on_data_received_handler 方法通过传入回调函数的方式来自定义处理接收到的数据。
+
         :param port:
         :param baud_rate:
         :param bytesize:
@@ -30,9 +30,9 @@ class SerialHelper:
         :param con2port_when_created:
         :param auto_search_port:
         """
-        assert self.find_serial_ports(), "No serial ports FOUND!"
+        assert find_serial_ports(), "No serial ports FOUND!"
         self._serial: Optional[Serial] = None
-        self._serial_port: str = port if port else self.find_serial_ports()[0]
+        self._serial_port: str = port if port else find_serial_ports()[0]
         self._baud_rate: int = baud_rate
         self._bytesize: int = bytesize
         self._parity: str = parity
@@ -44,7 +44,7 @@ class SerialHelper:
 
         self._read_thread_should_stop: Optional[bool] = None
         self._read_thread: Optional[Thread] = None
-        self._on_data_received_handler: Optional[Callable[[ByteString], Optional[Any]]] = None
+        self._on_data_handler: Optional[Callable[[ByteString], Optional[Any]]] = None
         if con2port_when_created:
             if self._serial_port:
                 self.connect(logging=True)
@@ -52,7 +52,7 @@ class SerialHelper:
                 # connection to the default has failed
                 # try to search for a new port
                 warnings.warn('Searching available Ports')
-                ports = self.find_serial_ports()
+                ports = find_serial_ports()
                 print(f'Available ports: {ports}')
                 for i in ports:
                     self.serial_port = i
@@ -198,71 +198,16 @@ class SerialHelper:
             warnings.warn("Connection is not set")
         return b''
 
-    @staticmethod
-    def find_serial_ports() -> List[str]:
-        return [port.device for port in serial.tools.list_ports.comports()]
-
-    @staticmethod
-    def find_usb_tty(id_product: int = 0, id_vendor: int = 0) -> List[str]:
+    def start_read_thread(self, interval: float = 0.1, read_buffer_size: int = 1024) -> None:
         """
-        该函数实现在 Linux 系统下查找指定厂商和产品 ID 的 USB 串口设备，并返回设备名列表。
-
-        具体实现方式是通过调用 serial.tools.list_ports.comports() 函数获取当前系统可用的串口设备信息，
-        检查每个设备名称中是否包含 "USB" 或者 "ACM" 关键字（因为基本上所有的 USB 转串口设备都会以这些字符开头），
-        然后进一步解析设备的 USB 相关属性信息来确定其所属厂商和产品类型。
-
-        此处使用了 Python 库 serial 提供的 list_ports.comports() 函数，
-        它的功能是获取当前系统的可用串口设备列表。调用该函数时，操作系统将扫描所有可能的串口设备号，
-        并尝试打开其中可用的端口，提取并返回它们的相关信息，包括设备名、描述信息、VID/PID 等。
-
-        Args:
-        id_vendor: 符合条件的 USB 设备的厂商 ID（默认为 0）
-        id_product: 符合条件的 USB 设备的产品 ID（默认为 0）
-
-        Returns:
-        一个字符串列表，包含所有符合条件的 USB to Serial 转换器设备名（如 '/dev/ttyUSB0'）
-
-        Examples:
-        usb = USBFinder()
-        tty_list = usb.find_usb_tty(id_vendor=1027, id_product=24577)
-        print(tty_list)
-
-        >> ['/dev/ttyUSB0', '/dev/ttyUSB1']
-
-        Note:
-        需要先安装 pyserial 模块，并且当前用户需要有足够权限访问串口设备。
-
-
+        Start the thread reading loop.
+        :param interval:
+        :param read_buffer_size:
+        :return:
         """
-        # 初始化结果列表
-        tty_list = []
-
-        # 查询系统中所有可用串口
-        for i in comports():
-            # 判断串口文件名是否包含有 "USB" 或 "ACM"
-            if 'USB' in i[0] or 'ACM' in i[0]:
-                # 读取串口设备的描述符信息（字符串格式）
-                info = i[2]
-                # 查找设备描述符中的 idVendor 和 idProduct 字段
-                vendor_id_loc = info.find('idVendor')
-                product_id_loc = info.find('idProduct')
-                # 如果设备描述符中存在产品 ID 和厂商 ID，则尝试解析出这两个值并进行匹配
-                if product_id_loc != -1 and vendor_id_loc != -1:
-                    vendor_id_str = info[vendor_id_loc:]
-                    product_id_str = info[product_id_loc:]
-                    vendor_id = int(vendor_id_str.split("=")[1].strip(), 16)
-                    product_id = int(product_id_str.split("=")[1].strip(), 16)
-                    if product_id == id_product and vendor_id == id_vendor:
-                        tty_list.append(i[0])  # 将符合条件的串口设备加入到结果列表中
-
-        # 返回结果列表
-        return tty_list
-
-    def start_read_thread(self, interval: float = 0.1, read_buffer_size: int = 512) -> None:
-        """Start the thread reading loop."""
         warnings.warn('##Start Read Thread##')
         self._read_thread_should_stop = False
-        self._read_thread = threading.Thread(target=self.__read_loop, args=(interval, read_buffer_size),
+        self._read_thread = threading.Thread(target=self._read_loop, args=(interval, read_buffer_size),
                                              name="read_thread")
         self._read_thread.daemon = True
         self._read_thread.start()
@@ -273,11 +218,11 @@ class SerialHelper:
         :param wait: Whether to wait for the thread to finish. Default is true.
         """
         self._read_thread_should_stop = True
-        if wait and self._read_thread is not None and threading.current_thread() != self._read_thread:
+        if wait and self._read_thread is not None:
             self._read_thread.join()
         self._read_thread = None
 
-    def __read_loop(self, interval: float, read_buffer_size: int) -> None:
+    def _read_loop(self, interval: float, read_buffer_size: int) -> None:
         """
         Thread loop that reads data from the serial port.
 
@@ -288,13 +233,72 @@ class SerialHelper:
         """
         while not self._read_thread_should_stop:
             data = self.read(read_buffer_size)
-            if len(data) > 0:
-                self._on_data_received_handler(data)
-            time.sleep(interval)
+            self._on_data_handler(data) if data else None
+            sleep(interval)
 
-    def set_on_data_received_handler(self, func: Callable[[ByteString], Optional[Any]]):
+    def set_data_handler(self, func: Callable[[ByteString], Optional[Any]]):
         """set serial data received callback"""
-        self._on_data_received_handler = func
+        self._on_data_handler = func
+
+
+def find_usb_tty(id_product: int = 0, id_vendor: int = 0) -> List[str]:
+    """
+    该函数实现在 Linux 系统下查找指定厂商和产品 ID 的 USB 串口设备，并返回设备名列表。
+
+    具体实现方式是通过调用 serial.tools.list_ports.comports() 函数获取当前系统可用的串口设备信息，
+    检查每个设备名称中是否包含 "USB" 或者 "ACM" 关键字（因为基本上所有的 USB 转串口设备都会以这些字符开头），
+    然后进一步解析设备的 USB 相关属性信息来确定其所属厂商和产品类型。
+
+    此处使用了 Python 库 serial 提供的 list_ports.comports() 函数，
+    它的功能是获取当前系统的可用串口设备列表。调用该函数时，操作系统将扫描所有可能的串口设备号，
+    并尝试打开其中可用的端口，提取并返回它们的相关信息，包括设备名、描述信息、VID/PID 等。
+
+    Args:
+    id_vendor: 符合条件的 USB 设备的厂商 ID（默认为 0）
+    id_product: 符合条件的 USB 设备的产品 ID（默认为 0）
+
+    Returns:
+    一个字符串列表，包含所有符合条件的 USB to Serial 转换器设备名（如 '/dev/ttyUSB0'）
+
+    Examples:
+    usb = USBFinder()
+    tty_list = usb.find_usb_tty(id_vendor=1027, id_product=24577)
+    print(tty_list)
+
+    >> ['/dev/ttyUSB0', '/dev/ttyUSB1']
+
+    Note:
+    需要先安装 pyserial 模块，并且当前用户需要有足够权限访问串口设备。
+
+
+    """
+    # 初始化结果列表
+    tty_list = []
+
+    # 查询系统中所有可用串口
+    for i in comports():
+        # 判断串口文件名是否包含有 "USB" 或 "ACM"
+        if 'USB' in i[0] or 'ACM' in i[0]:
+            # 读取串口设备的描述符信息（字符串格式）
+            info = i[2]
+            # 查找设备描述符中的 idVendor 和 idProduct 字段
+            vendor_id_loc = info.find('idVendor')
+            product_id_loc = info.find('idProduct')
+            # 如果设备描述符中存在产品 ID 和厂商 ID，则尝试解析出这两个值并进行匹配
+            if product_id_loc != -1 and vendor_id_loc != -1:
+                vendor_id_str = info[vendor_id_loc:]
+                product_id_str = info[product_id_loc:]
+                vendor_id = int(vendor_id_str.split("=")[1].strip(), 16)
+                product_id = int(product_id_str.split("=")[1].strip(), 16)
+                if product_id == id_product and vendor_id == id_vendor:
+                    tty_list.append(i[0])  # 将符合条件的串口设备加入到结果列表中
+
+    # 返回结果列表
+    return tty_list
+
+
+def find_serial_ports() -> List[str]:
+    return [port.device for port in serial.tools.list_ports.comports()]
 
 
 class DummyLock:
