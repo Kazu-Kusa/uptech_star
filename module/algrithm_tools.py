9d945ae7a2c2fd6e384c4a84d18d228e4dfea920
Whth <88489697+Whth@users.noreply.github.com>
2023-08-15 01:56:05 +0800

    feat(添加9类multiplier随机生成器):

--- module/algrithm_tools.py
+++ module/algrithm_tools.py
@@ -12,7 +12,7 @@ class BaseFilter(ABC):
 
 
 class MovingAverage(BaseFilter):
-    
+
     def apply(self, value: float) -> float:
         self.queue[:-1] = self.queue[1:]  # 将队列往前移动
         self.queue[-1] = value  # 将最新的值添加到队列中
@@ -47,20 +47,103 @@ def multiply(factor_1: Union[float, int], factor_2: Union[float, int]) -> int:
 
 
 def random_sign() -> int:
+    """
+    Generate a random sign (-1 or 1).
+
+    Returns:
+        int: The randomly generated sign (-1 or 1).
+    """
     return choice([-1, 1])
 
 
-def random_enlarge_multiplier() -> float:
-    return choice([1.50, 1.55, 1.60, 1.65, 1.70, 1.75, 1.80, 1.85, 1.90, 1.95, 2.00])
+def enlarge_multiplier_l() -> float:
+    """
+    Generate a multiplier for enlargement in a lower range.
+
+    Returns:
+        float: The randomly generated multiplier for enlargement in a lower range.
+    """
+    return choice([1.25, 1.275, 1.3, 1.325, 1.35, 1.375, 1.4, 1.45, 1.5])
 
 
-def random_shrink_multiplier() -> float:
-    return choice([0.5, 0.525, 0.55, 0.575, 0.6, 0.625, 0.65, ])
+def enlarge_multiplier_ll() -> float:
+    """
+    Generate a multiplier for enlargement in a middle range.
+
+    Returns:
+        float: The randomly generated multiplier for enlargement in a middle range.
+    """
+    return choice([1.5, 1.525, 1.55, 1.575, 1.6, 1.625, 1.65, 1.7, 1.75])
 
 
-def random_float_multiplier() -> float:
-    return choice([0.75, 0.775, 0.8, 0.825, 0.85, 0.875, 0.9, 0.925, 0.95, 0.975,
-                   1.0, 1.025, 1.05, 1.075, 1.1, 1.125, 1.15, 1.175, 1.2, 1.225, 1.25])
+def enlarge_multiplier_lll() -> float:
+    """
+    Generate a multiplier for enlargement in an upper range.
+
+    Returns:
+        float: The randomly generated multiplier for enlargement in an upper range.
+    """
+    return choice([1.75, 1.775, 1.8, 1.825, 1.85, 1.875, 1.9, 1.95, 2.0, 2.1, 2.2, 2.4])
+
+
+def shrink_multiplier_l() -> float:
+    """
+    Generate a multiplier for shrinkage in a lower range.
+
+    Returns:
+        float: The randomly generated multiplier for shrinkage in a lower range.
+    """
+    return choice([0.65, 0.7, 0.725, 0.7375, 0.75, 0.7625, 0.775, 0.7875, 0.8])
+
+
+def shrink_multiplier_ll() -> float:
+    """
+    Generate a multiplier for shrinkage in a middle range.
+
+    Returns:
+        float: The randomly generated multiplier for shrinkage in a middle range.
+    """
+    return choice([0.55, 0.6, 0.625, 0.65, 0.6625, 0.675, 0.6875, 0.7])
+
+
+def shrink_multiplier_lll() -> float:
+    """
+    Generate a multiplier for shrinkage in an upper range.
+
+    Returns:
+        float: The randomly generated multiplier for shrinkage in an upper range.
+    """
+    return choice([0.40, 0.5, 0.525, 0.55, 0.5625, 0.575, 0.5875, 0.6])
+
+
+def float_multiplier_middle() -> float:
+    """
+    Generate a float multiplier in a middle range.
+
+    Returns:
+        float: The randomly generated float multiplier in a middle range.
+    """
+    return choice([0.8, 0.85, 0.9, 0.95, 1.0, 1.05, 1.1, 1.17, 1.25])
+
+
+def float_multiplier_lower() -> float:
+    """
+    Generate a float multiplier in a lower range.
+
+    Returns:
+        float: The randomly generated float multiplier in a lower range.
+    """
+    return choice([0.8, 0.825, 0.85, 0.875, 0.9, 0.925, 0.95, 1.0, 1.05, 1.08, 1.1])
+
+
+def float_multiplier_upper() -> float:
+    """
+    Generate a float multiplier in an upper range.
+
+    Returns:
+        float: The randomly generated float multiplier in an upper range.
+    """
+    return choice([0.9, 0.925, 0.95, 1.0, 1.05, 1.08, 1.1, 1.17, 1.25])
 
 
 def calc_p2p_dst(point_1: Tuple[int | float, int | float], point_2: Tuple[int | float, int | float]) -> float:
